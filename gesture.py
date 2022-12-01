31095689e55e0d665f0e6d21b96e833a37dfd134
AsifAz07 <63720283+AsifAz07@users.noreply.github.com>
2022-12-01 14:40:30 +0530

    Add files via upload

---
+++ gesture.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+mp_drawing=mp.solutions.drawing_utils
+mp_drawing_styles=mp.solutions.drawing_styles
+mphands=mp.solutions.hands
+
+
+cap=cv2.VideoCapture(0)
+hands=mphands.Hands()
+while True:
+    data,image=cap.read()
+    image=cv2.cvtColor(cv2.flip(image,1),cv2.COLOR_BGR2RGB)
+    results=hands.process(image)
+    image=cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(image, hand_landmarks,mphands.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(image, hand_landmarks,mphands.HAND_CONNECTIONS)
+
+    cv2.imshow("HANDTRACKING", image)
+    if cv2.waitKey(1) == ord('q'):
+        break
+        
+cap.release()
+
+cv2.destroyAllWindows()
+    
